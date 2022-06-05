554a0ed7b7c935376dc6eac18d99d728e8879b16
Flor.Stein <mfrsteinfeld@gmail.com>
2022-06-05 20:50:43 -0300

    cambio de animaciones y background

--- index/acc.js
+++ index/acc.js
@@ -3,28 +3,29 @@
 
 const accordionDiv =   document.querySelectorAll('.accordion-div')
 const services     =   document.querySelectorAll('.services')
-const card         =   document.querySelectorAll('.card')
+// const card         =   document.querySelectorAll('.card')
 const cartel       =   document.querySelectorAll('.hidden-cartel')
 const divider      =   document.getElementById('divider')
+const divisor      =   document.getElementById('divisor')
 const bgr0         =   document.getElementById('bgr0')
 const bgr1         =   document.getElementById('bgr1')
 const bgr2         =   document.getElementById('bgr2')
 const bgr3         =   document.getElementById('bgr3')
 const bgr4         =   document.getElementById('bgr4')
 const bgr5         =   document.getElementById('bgr5')
-const card0         =   document.getElementById('card0')
-const card1         =   document.getElementById('card1')
-const card2         =   document.getElementById('card2')
-const card3         =   document.getElementById('card3')
-const card4         =   document.getElementById('card4')
-const card5         =   document.getElementById('card5')
+const card0        =   document.getElementById('card0')
+const card1        =   document.getElementById('card1')
+const card2        =   document.getElementById('card2')
+const card3        =   document.getElementById('card3')
+const card4        =   document.getElementById('card4')
+const card5        =   document.getElementById('card5')
 
 
 
-accordionDiv.forEach( ( cadaAccordionDiv , i )=>{
+accordionDiv.forEach( ( _cadaAccordionDiv , i )=>{
     accordionDiv[i].addEventListener('mouseover', ()=>{
 
-        services.forEach( ( cadaServices, i)=>{
+        services.forEach( ( _cadaServices, i)=>{
             services[i].classList.remove('active')
 
         })
@@ -34,10 +35,10 @@ accordionDiv.forEach( ( cadaAccordionDiv , i )=>{
 })
 
 
-accordionDiv.forEach( ( cadaAccordionDiv , i )=>{
+accordionDiv.forEach( ( _cadaAccordionDiv , i )=>{
     accordionDiv[i].addEventListener('mouseover', ()=>{
 
-        cartel.forEach( ( cadaServices, i)=>{
+        cartel.forEach( ( _cadaServices, i)=>{
             cartel[i].classList.remove('cartel')
 
         })
@@ -107,6 +108,36 @@ card5.addEventListener('mouseover',() =>{
     divider.classList.remove('bgr-1','bgr1','bgr2','bgr3','bgr4','bgr0')
 })
 
+
+card0.addEventListener('mouseover',() =>{
+    divisor.classList.add('bgr0')
+    divisor.classList.remove('bgr-1','bgr1','bgr2','bgr3','bgr4','bgr5')
+})
+
+card1.addEventListener('mouseover',() =>{
+    divisor.classList.add('bgr1')
+    divisor.classList.remove('bgr-1','bgr0','bgr2','bgr3','bgr4','bgr5')
+})
+
+card2.addEventListener('mouseover',() =>{
+    divisor.classList.add('bgr2')
+    divisor.classList.remove('bgr-1','bgr1','bgr0','bgr3','bgr4','bgr5')
+})
+
+card3.addEventListener('mouseover',() =>{
+    divisor.classList.add('bgr3')
+    divisor.classList.remove('bgr-1','bgr1','bgr2','bgr0','bgr4','bgr5')
+})
+
+card4.addEventListener('mouseover',() =>{
+    divisor.classList.add('bgr4')
+    divisor.classList.remove('bgr-1','bgr1','bgr2','bgr3','bgr0','bgr5')
+})
+
+card5.addEventListener('mouseover',() =>{
+    divisor.classList.add('bgr5')
+    divisor.classList.remove('bgr-1','bgr1','bgr2','bgr3','bgr4','bgr0')
+})
 
 
 
